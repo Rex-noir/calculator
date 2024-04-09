3e8aaa5910774c40525a024c16304cd32a43e0f6
AceReX <ace.rexnoir@gmail.com>
2024-04-09 17:47:36 +0530

    Overflow and buttons clicks need to be fixed

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import { addNumbers } from "./functions.js";
 
 let buttons = document.querySelectorAll(".button");
 buttons.forEach((button)=>{
-    button.addEventListener("click", keyPressed)});
+    button.addEventListener("click", keyPressed)
+});
+
+let result = document.querySelector('.result-text');
 
 //Key pressed
 function keyPressed(e){
+    let element = e.target;
+    //clear result first
+    result.textContent += String(element.textContent);
 }
