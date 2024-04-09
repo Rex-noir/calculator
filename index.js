ce74237cd710f5af0ff8e586f8e9853260119d40
AceReX <ace.rexnoir@gmail.com>
2024-04-09 17:14:24 +0530

    Add eventListener for all buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+import { addNumbers } from "./functions.js";
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach((button)=>{
+    button.addEventListener("click", keyPressed)});
+
+//Key pressed
+function keyPressed(e){
+}
